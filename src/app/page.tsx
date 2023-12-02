dc31b172862088b0b1e9e57c28653e4c46c766a4
oboutarf <osc.boutarfa@gmail.com>
2023-12-02 15:41:34 +0100

    [starting landing page project]

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+import Home from '@/components/home/home';
+
+export default function HomeServerAppRouter({
+
+} : {
+
+})  {
+
+  return (<Home />);
+};
---
+++ src/components/home/home.tsx
@@ -0,0 +1,11 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+export default function Home({
+
+}: {
+
+})  {
+    return (
+        <></>
+    );
+};
